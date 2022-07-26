28318440087c644d4a937f7cfa7d8a4881f01b21
Hadi Abdurrahman <haddi.rachman@gmail.com>
2022-07-26 12:20:05 +0700

    Update absen.py

--- absen.py
+++ absen.py
@@ -48,7 +48,6 @@ nip = [
     [197601132006041001, "Januari1976", "WFO"],  # M
 ]
 
-
 def skemaraja():
     #     masuk ke web absen
     try:
@@ -57,67 +56,108 @@ def skemaraja():
         browser.quit()
         skemaraja()
 
-
 def WFO():
-    # opsi bila WFH
-    browser.find_element_by_xpath(
-        '//*[@id="theForm"]/div/p[3]/select/option[2]'
+    # opsi bila WFO
+    browser.find_element(By.XPATH,
+        '//*[@id="theForm"]/div/p[3]/select/option[3]'
     ).click()
     print("Select WFO     [ berhasil ]")
-
-    # opsi bila masuk Shift 1
-    browser.find_element_by_xpath('//*[@id="shift_1"]').click()
-    print("Select Shift 1 [ berhasil ]")
     # time.sleep(1)
 
     # opsi bila masuk Shift 2
-    # browser.find_element_by_xpath('//*[@id="shift_2"]').click()
-
+    # browser.find_element(By.XPATH,'//*[@id="shift_2"]').click()
+    
+    # opsi bila masuk Shift 1
+    browser.find_element(By.XPATH, '//*[@id="shift_1"]').click()
+    print("Select Shift 1 [ berhasil ]")
 
 def WFH():
-    # opsi bila WFO
-    browser.find_element_by_xpath(
-        '//*[@id="theForm"]/div/p[3]/select/option[1]'
+    # opsi bila WFH
+    browser.find_element(By.XPATH,
+        '//*[@id="theForm"]/div/p[3]/select/option[2]'
     ).click()
     print("Select WFH     [ berhasil ]")
-    # time.sleep(1)
-
 
 def DL():
     # opsi bila DL
-    browser.find_element_by_xpath(
-        '//*[@id="theForm"]/div/p[3]/select/option[3]'
+    browser.find_element(By.XPATH,
+        '//*[@id="theForm"]/div/p[3]/select/option[4]'
     ).click()
     print("Select DL      [ berhasil ]")
-    # time.sleep(1)
 
+def resiko():
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[3]/button"
+    ).click()
+    print("Form Resiko    [ berhasil ]")
 
-# definisi absen
-def absen(nip, pas, st):
+def gejala():
+    #   print(browser.find_element(By.CLASS_NAME, "brand-image").text)
+    #   browser.execute_script("document.body.style.zoom='50%'")
+    browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    browser.find_element(By.NAME, "p1_suhu").send_keys("36.5")
+    browser.find_element(By.XPATH, 
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[1]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[2]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[3]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[4]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[5]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[6]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[7]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[8]/td[4]/input"
+    ).click()
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[9]/td[4]/input"
+    ).click()
+    #time.sleep(1)
+    browser.find_element(By.XPATH,
+        "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[3]/button"
+    ).click()
+    print("Form Gejala    [ berhasil ]")
+
+    def absen(nip, pas, st):
     skemaraja()
 
     # input NIP
-    browser.find_element_by_name("nip").send_keys(nip)
+    browser.find_element(By.NAME, "nip").send_keys(nip)
+    #browser.find_element_by_name("nip").send_keys(nip)
     print("Input NIP      [ berhasil ]")
     # time.sleep(1)
 
     # input pass
-    browser.find_element_by_name("password").send_keys(pas)
+    browser.find_element(By.NAME, "password").send_keys(pas)
+    #browser.find_element_by_name("password").send_keys(pas)
     print("Input Pass     [ berhasil ]")
     # time.sleep(1)
 
+
     if st == "WFO":
         WFO()
-    elif st == "WFH":
+    if st == "WFH":
         WFH()
-    elif st == "DL":
+    if st == "DL":
         DL()
     else:
         print("salah")
 
     # klik submit
     try:
-        browser.find_element_by_id("btnSubmit").click()
+        browser.find_element(By.ID, "btnSubmit").click()
+        #browser.find_element_by_id("btnSubmit").click()
         print("Login          [ berhasil ]")
     except NoSuchElementException:
         browser.refresh()
@@ -125,94 +165,16 @@ def absen(nip, pas, st):
     # time.sleep(0.3)
 
     # testing add Exception by hadi
-    try:
-        browser.find_element_by_name("p1_suhu").send_keys("36.5")
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[1]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[2]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[3]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[4]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[5]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[6]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[7]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[8]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[9]/td[4]/input"
-        ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[3]/button"
-        ).click()
-        print("Submit 1       [ OPSI 2 ]")
-    except NoSuchElementException:
-        print("Submit 1       [ terlewat ]")
-
-    # time.sleep(0.3)
-    try:
-        # browser.find_element_by_name("p1_suhu").send_keys("36.5")
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[1]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[2]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[3]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[4]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/di
-        # v/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[5]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[6]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[7]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[8]/td[4]/input"
-        # ).click()
-        # browser.find_element_by_xpath(
-        #     "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[2]/div/div/table/tbody/tr[9]/td[4]/input"
-        # ).click()
-        browser.find_element_by_xpath(
-            "/html/body/div/div/div[2]/div/div[2]/div/form/div/div[3]/button"
-        ).click()
-        print("Submit 2       [ OPSI 1 ]")
-    except NoSuchElementException:
-        print("Submit 2       [ terlewat ]")
-        # time.sleep(3)
-
-    # browser.find_element_by_class_name("profile-username text-center").get_property()
-    # keluar dari absen
-    try:
-        browser.get("https://skemaraja.dephub.go.id/logout")
-        #    browser.quit()
-        # try:
-        #    browser.find_element_by_link_text("Check Out").click()
-        print("Logout         [ berhasil ]")
-    except NoSuchElementException:
-        browser.quit()
-        skemaraja()
-        print("Logout         [ refresh  ]")
-    # time.sleep(0.3)
+    form = browser.find_element(By.CSS_SELECTOR, ".text-sm .card-title").text
+    if form == "Form Resiko":
+        resiko()
+        gejala()
+        login()
+    if form == "Form Gejala":
+        gejala()
+        login()
+    else:
+        login()
 
 
 # looping nip dan pass absen
